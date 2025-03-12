063052056cbeec3fee9360e38ff31e8d7486a00e
TaxRich <148796696+TaxRich@users.noreply.github.com>
2025-03-12 22:34:38 +0530

    Update main.py

--- calories/main.py
+++ calories/main.py
@@ -3,6 +3,6 @@ from src.predict import predict_calories
 
 if __name__ == "__main__":
     train_model()
-    sample_input = [1, 30, 150, 60, 10]  # Example input
+    sample_input = [1, 30, 150, 60, 10,5, 3]  # Example input
     result = predict_calories(sample_input)
     print(f"Predicted Calories Burned: {result}")
